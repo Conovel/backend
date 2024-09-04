8f3d6468ca1ac4c30dbf2211b981a32420b2bfae
ryo-i <iidazz0109@gmail.com>
2024-09-05 06:52:59 +0900

    コメント追加（rubocopエラー修正）

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 module ActiveSupport
+  # This is a test case for ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
