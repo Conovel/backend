043ee323cdec3176d7ce7754f53bc540a02c8eb5
ryo-i <iidazz0109@gmail.com>
2024-09-23 14:25:35 +0900

    CORSのurlをproductionとdevelopmentで定義分け

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set CORS origin URL
+  config.origin_url = ENV['PRODUCTION_ORIGIN_URL'] || 'https://conovel.jp'
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' # localhost frontend url
+    origins Rails.application.config.origin_url
 
     resource '*',
              headers: :any,
