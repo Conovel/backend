91ee371e0d8b37533e4c9c9d7fb2688ab3e49eaf
ryo-i <iidazz0109@gmail.com>
2024-12-15 18:02:35 +0900

    createのテストを追加

--- spec/requests/sentences_spec.rb
+++ spec/requests/sentences_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Sentences', type: :request do
+  # showのテスト
   let!(:users) { create_list(:user, 4) }
   let!(:title) { create(:title, author_user: users[0]) }
   let!(:parent_sentence) do
@@ -34,6 +35,15 @@ RSpec.describe 'Sentences', type: :request do
                                               parent_sentence:, hierarchy: 2)
   end
 
+  # createのテスト
+  let(:valid_attributes) do
+    {
+      parent_sentence_id: parent_sentence.sentence_id,
+      sentence: '投稿追加テストです。',
+      parent_updated_at: parent_sentence.updated_at
+    }
+  end
+
   describe 'GET /v1/sentences/:sentence_id' do
     it 'returns the sentence' do
       main_sentence
@@ -55,6 +65,71 @@ RSpec.describe 'Sentences', type: :request do
       expect(json_response).to have_key('parallels')
       expect(json_response['parallels'][0]).not_to be_nil
       expect(json_response['parallels'][0]['sentence']).to eq('かかかかか')
+    end
+  end
+
+  describe 'POST /v1/sentences' do
+    context 'with valid parameters' do
+      it 'creates a new Sentence' do
+        expect do
+          post v1_sentences_path, params: valid_attributes
+        end.to change(Sentence, :count).by(1)
+        expect(response).to have_http_status(:created)
+        json_response = JSON.parse(response.body)
+        expect(json_response['main']['sentence']).to eq('投稿追加テストです。')
+      end
+    end
+
+    context 'when required parameters are missing' do
+      it 'returns an unprocessable entity status' do
+        post v1_sentences_path, params: valid_attributes.merge(sentence: '')
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']['code']).to eq(422)
+        expect(json_response['error']['message']).to eq('必須項目に空欄があります。')
+      end
+    end
+
+    context 'when parent_sentence_id does not exist' do
+      it 'returns an unprocessable entity status' do
+        post v1_sentences_path, params: valid_attributes.merge(parent_sentence_id: 100)
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']['code']).to eq(422)
+        expect(json_response['error']['message']).to eq('親投稿が見つかりません')
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'returns a conflict status' do
+        post v1_sentences_path, params: valid_attributes.merge(parent_updated_at: '2024-01-01T01:01:09.292+09:00')
+        expect(response).to have_http_status(:conflict)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']['code']).to eq(409)
+        expect(json_response['error']['message']).to eq('投稿編集の途中で親投稿が編集されたため、投稿を保留しています')
+      end
+    end
+
+    context 'when consecutive self post is detected' do
+      it 'returns an unprocessable entity status' do
+        post_user_id = 2 # Google認証未実装のため、仮のユーザーID
+        post v1_sentences_path, params: valid_attributes.merge(parent_sentence_id: post_user_id)
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']['code']).to eq(422)
+        expect(json_response['error']['message']).to eq('自分自身の投稿の後に連続で投稿を追加することはできません。')
+      end
+    end
+
+    context 'when sentence length exceeds the limit' do
+      it 'returns an unprocessable entity status' do
+        long_sentence = 'a' * 101
+        post v1_sentences_path, params: valid_attributes.merge(sentence: long_sentence)
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']['code']).to eq(422)
+        expect(json_response['error']['message']).to eq('投稿文字数の上限を超えています。修正後に再投稿をお願いします。')
+      end
     end
   end
 end
