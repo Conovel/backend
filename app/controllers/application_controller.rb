9f7fe0933bd425d9140604e1617d0e70aa388466
ryo-i <iidazz0109@gmail.com>
2025-01-12 17:46:11 +0900

    Rails.logger.errorを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,16 +32,19 @@ class ApplicationController < ActionController::API
 
   # ArgumentError の場合
   def handle_argument_error(exception)
+    Rails.logger.error "ArgumentError: #{exception.message}\n#{exception.backtrace.join("\n")}"
     render_error_response(422, "無効な値が含まれていました。: #{exception.message}")
   end
 
   # RecordInvalid の場合
   def handle_record_invalid(exception)
+    Rails.logger.error "RecordInvalid: #{exception.message}\n#{exception.backtrace.join("\n")}"
     render_error_response(422, record_invalid_message(exception))
   end
 
   # CustomError の場合
   def handle_custom_error(exception)
+    Rails.logger.error "CustomError: #{exception.message}\n#{exception.backtrace.join("\n")}"
     render_error_response(exception.code, exception.message, data: exception.data)
   end
 
