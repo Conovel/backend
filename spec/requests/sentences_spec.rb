33abdfdbb1e84ddc383229c2d5d3f423d22a2803
ryo-i <iidazz0109@gmail.com>
2025-01-29 08:33:23 +0900

    rspecにViewedSentenceのレコード追加が失敗するケースを追加

--- spec/requests/sentences_spec.rb
+++ spec/requests/sentences_spec.rb
@@ -73,6 +73,16 @@ RSpec.describe 'Sentences', type: :request do
         expect(json_response['parallels'][0]).not_to be_nil
         expect(json_response['parallels'][0]['sentence']).to eq('かかかかか')
       end
+
+      it 'returns a 420 error when viewed_sentence save fails' do
+        main_sentence
+        allow_any_instance_of(ViewedSentence).to receive(:save!).and_raise(StandardError.new('DB error'))
+
+        get "/v1/sentences/#{main_sentence.sentence_id}"
+        expect(response).to have_http_status(420)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']['message']).to eq('投稿の取得に失敗しました。')
+      end
     end
 
     context 'when the sentence does not exist' do
