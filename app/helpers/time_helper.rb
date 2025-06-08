87aef3694ae8f7d9094b668cb5960ec171bdc9cb
ryo-i <iidazz0109@gmail.com>
2025-06-08 14:12:15 +0900

    生年月の処理に日本時間フォーマットを追加（ヘルパー関数を使用）

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -3,12 +3,12 @@
 # TimeHelper
 module TimeHelper
   # 時刻をフォーマット（日本時間）
-  def format_time(time)
+  def format_time(time = Time.zone.now)
     time.in_time_zone('Asia/Tokyo')
   end
 
   # 時刻をフォーマット（日本時間）して文字列として返す
-  def format_time_with_strftime(time)
+  def format_time_with_strftime(time = Time.zone.now)
     format_time(time).strftime('%Y-%m-%d %H:%M:%S %z')
   end
 
@@ -20,5 +20,10 @@ module TimeHelper
   # テキストを時刻にしてからフォーマット（日本時間）して文字列として返す
   def format_time_from_string_with_strftime(time_str)
     format_time(DateTime.parse(time_str)).strftime('%Y-%m-%d %H:%M:%S %z')
+  end
+
+  # 時刻をフォーマット（日本時間）して年月（例: "202406"）を返す
+  def format_year_month(time = Time.zone.now)
+    format_time(time).strftime('%Y%m')
   end
 end
