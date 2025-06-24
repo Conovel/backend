c0843b8bc915b1873740aa001a4c48ffc2b57d72
ryo-i <iidazz0109@gmail.com>
2025-06-25 08:38:31 +0900

    Googleログインエラー時のリダイレクトは復活

--- app/controllers/v1/auth_controller.rb
+++ app/controllers/v1/auth_controller.rb
@@ -150,6 +150,7 @@ module V1
       delete_tokens
       error_message = request.env['omniauth.error.type'] || 'Unknown error'
       # アカウント画面にリダイレクト
+      redirect_to "#{FRONTEND_URL}/login", allow_other_host: true
       Rails.logger.error("[ERROR] 認証エラーが発生しました。再度お試しください。: #{error_message}")
     end
 
