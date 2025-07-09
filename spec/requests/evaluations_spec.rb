76dbb89013e0869279a4526ec6da0ce2ce894070
ryo-i <iidazz0109@gmail.com>
2025-07-10 08:20:24 +0900

    evaluationsのリクエスト・レスポンスをキャメルケースに変更

--- spec/requests/evaluations_spec.rb
+++ spec/requests/evaluations_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Evaluations', type: :request do
   let!(:sentence) { create(:sentence) }
 
   describe 'POST /v1/evaluations' do
-    let(:valid_attributes) { { sentence_id: sentence.sentence_id, evaluation: 'good' } }
+    let(:valid_attributes) { { sentenceId: sentence.sentence_id, evaluation: 'good' } }
 
     before do
       # クッキーにJWTトークンを設定
@@ -18,9 +18,9 @@ RSpec.describe 'Evaluations', type: :request do
       it 'creates a new evaluation' do
         post('/v1/evaluations', params: valid_attributes)
         expect(response).to have_http_status(:created)
-        expect(json['sentence_id'].to_i).to eq(sentence.sentence_id)
-        expect(json['evaluation_good_count']).to eq(1)
-        expect(json['evaluation_stay_count']).to eq(0)
+        expect(json['sentenceId'].to_i).to eq(sentence.sentence_id)
+        expect(json['evaluationGoodCount']).to eq(1)
+        expect(json['evaluationStayCount']).to eq(0)
       end
     end
 
@@ -28,9 +28,9 @@ RSpec.describe 'Evaluations', type: :request do
       it 'creates a new evaluation' do
         post('/v1/evaluations', params: valid_attributes.merge(evaluation: 'stay'))
         expect(response).to have_http_status(:created)
-        expect(json['sentence_id'].to_i).to eq(sentence.sentence_id)
-        expect(json['evaluation_good_count']).to eq(0)
-        expect(json['evaluation_stay_count']).to eq(1)
+        expect(json['sentenceId'].to_i).to eq(sentence.sentence_id)
+        expect(json['evaluationGoodCount']).to eq(0)
+        expect(json['evaluationStayCount']).to eq(1)
       end
     end
 
@@ -38,9 +38,9 @@ RSpec.describe 'Evaluations', type: :request do
       it 'creates a new evaluation' do
         post('/v1/evaluations', params: valid_attributes.merge(evaluation: 'bad'))
         expect(response).to have_http_status(:created)
-        expect(json['sentence_id'].to_i).to eq(sentence.sentence_id)
-        expect(json['evaluation_good_count']).to eq(0)
-        expect(json['evaluation_stay_count']).to eq(0)
+        expect(json['sentenceId'].to_i).to eq(sentence.sentence_id)
+        expect(json['evaluationGoodCount']).to eq(0)
+        expect(json['evaluationStayCount']).to eq(0)
       end
     end
 
@@ -49,15 +49,15 @@ RSpec.describe 'Evaluations', type: :request do
         post('/v1/evaluations', params: valid_attributes)
         post('/v1/evaluations', params: valid_attributes.merge(evaluation: 'stay'))
         expect(response).to have_http_status(:created)
-        expect(json['sentence_id'].to_i).to eq(sentence.sentence_id)
-        expect(json['evaluation_good_count']).to eq(0)
-        expect(json['evaluation_stay_count']).to eq(1)
+        expect(json['sentenceId'].to_i).to eq(sentence.sentence_id)
+        expect(json['evaluationGoodCount']).to eq(0)
+        expect(json['evaluationStayCount']).to eq(1)
       end
     end
 
-    context 'When sentence_id does not exist' do
+    context 'When sentenceId does not exist' do
       it 'returns a validation failure message' do
-        post('/v1/evaluations', params: valid_attributes.merge(sentence_id: 1000))
+        post('/v1/evaluations', params: valid_attributes.merge(sentenceId: 1000))
         json_response = JSON.parse(response.body)
         expect(response).to have_http_status(:unprocessable_entity)
         expect(json_response['error']['code']).to eq(422)
@@ -65,9 +65,9 @@ RSpec.describe 'Evaluations', type: :request do
       end
     end
 
-    context 'When sentence_id is invalid' do
+    context 'When sentenceId is invalid' do
       it 'returns a validation failure message' do
-        post('/v1/evaluations', params: valid_attributes.merge(sentence_id: 'aaa'))
+        post('/v1/evaluations', params: valid_attributes.merge(sentenceId: 'aaa'))
         json_response = JSON.parse(response.body)
         expect(response).to have_http_status(:unprocessable_entity)
         expect(json_response['error']['code']).to eq(422)
@@ -75,9 +75,9 @@ RSpec.describe 'Evaluations', type: :request do
       end
     end
 
-    context 'When sentence_id is blank' do
+    context 'When sentenceId is blank' do
       it 'returns a validation failure message' do
-        post('/v1/evaluations', params: valid_attributes.merge(sentence_id: nil))
+        post('/v1/evaluations', params: valid_attributes.merge(sentenceId: nil))
         json_response = JSON.parse(response.body)
         expect(response).to have_http_status(:unprocessable_entity)
         expect(json_response['error']['code']).to eq(422)
