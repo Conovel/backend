67bbb237a27184ae6a97c8cb2fce93a11b0d8e87
ryo-i <iidazz0109@gmail.com>
2025-09-21 18:02:40 +0900

    user_display_info に nick_name と匿名/非匿名テストを追加

--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -16,5 +16,17 @@ RSpec.describe UserHelper, type: :helper do
       result = helper.user_display_info(user)
       expect(result).to include(:pen_name, :nick_name, :profile_icon_image)
     end
+
+    it 'returns anonymous display when user is anonymous' do
+      user = build_stubbed(:user, is_anonymous: true)
+      expect(helper.user_display_info(user)).to eq({ pen_name: anonymous, nick_name: anonymous,
+                                                     profile_icon_image: '' })
+    end
+
+    it 'returns actual pen/nick/profile for normal user' do
+      user = build_stubbed(:user, pen_name: 'Pen', nick_name: 'Nick', profile_icon_image: 'icon.png')
+      expect(helper.user_display_info(user)).to eq({ pen_name: 'Pen', nick_name: 'Nick',
+                                                     profile_icon_image: 'icon.png' })
+    end
   end
 end
